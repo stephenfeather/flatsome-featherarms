9baa9dcc160e9c20be8df5b3c7443e782a0312cd
root <root@featherarms.com>
2022-11-25 13:18:38 -0500

    redirect image attachments to their parent

---
+++ image.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * The template for displaying image attachments.
+ *
+ * @package          Flatsome\Templates
+ * @flatsome-version 3.16.0
+ */
+wp_redirect(get_permalink($post->post_parent)); ?>
